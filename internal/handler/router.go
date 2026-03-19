303a8acb9a6d21d020d9d048884445efd71cfa1a
vla8islav <vladislav888@gmail.com>
2026-03-20 02:09:41 +0300

    iter7 add /update and /value

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -8,6 +8,8 @@ func NewRouter(handler *Handler) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/update/{metricType}/{metricName}/{metricValue}", handler.SetMetrics)
 	r.HandleFunc("/value/{metricType}/{metricName}", handler.GetMetrics)
+	r.HandleFunc("/update", handler.SetMetricsUniversal)
+	r.HandleFunc("/value", handler.GetMetricsUniversal)
 	r.HandleFunc("/", handler.GetAllMetrics)
 	return r
 }
