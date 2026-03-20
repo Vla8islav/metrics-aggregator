1730f70ae98da2ddf67cee7919ecf734be4ba217
vla8islav <vladislav888@gmail.com>
2026-03-20 06:34:59 +0300

    iter7 added routing for /update and /updade/ as well as /value and /value/

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -8,7 +8,9 @@ func NewRouter(handler *Handler) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/update/{metricType}/{metricName}/{metricValue}", handler.SetMetrics)
 	r.HandleFunc("/value/{metricType}/{metricName}", handler.GetMetrics)
+	r.HandleFunc("/update/", handler.SetMetricsUniversal)
 	r.HandleFunc("/update", handler.SetMetricsUniversal)
+	r.HandleFunc("/value/", handler.GetMetricsUniversal)
 	r.HandleFunc("/value", handler.GetMetricsUniversal)
 	r.HandleFunc("/", handler.GetAllMetrics)
 	return r
