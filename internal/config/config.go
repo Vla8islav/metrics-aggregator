0be0f0d59779aeeef2c4b1ff2fec930c4933c637
vla8islav <vladislav888@gmail.com>
2026-03-21 01:18:30 +0300

    iter8

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,6 +12,10 @@ type Options struct {
 	ServerAddress  string                `env:"ADDRESS"`
 	PollInterval   CustomSecondsDuration `env:"POLL_INTERVAL"`
 	ReportInterval CustomSecondsDuration `env:"REPORT_INTERVAL"`
+
+	StoreInterval   CustomSecondsDuration `env:"STORE_INTERVAL"`
+	FileStoragePath string                `env:"FILE_STORAGE_PATH"`
+	Restore         bool                  `env:"RESTORE"`
 }
 
 var optionsInstance *Options
@@ -43,6 +47,18 @@ func mergeOptions(mergeInto *Options, newValues Options) {
 	if mergeInto.ReportInterval.Duration == 0 && newValues.ReportInterval.Duration != 0 {
 		mergeInto.ReportInterval = newValues.ReportInterval
 	}
+
+	if mergeInto.StoreInterval.Duration == 0 && newValues.StoreInterval.Duration != 0 {
+		mergeInto.StoreInterval = newValues.StoreInterval
+	}
+
+	if mergeInto.FileStoragePath == "" && newValues.FileStoragePath != "" {
+		mergeInto.FileStoragePath = newValues.FileStoragePath
+	}
+
+	if mergeInto.Restore && newValues.ReportInterval.Duration != 0 {
+		mergeInto.ReportInterval = newValues.ReportInterval
+	}
 }
 
 func getEnvOptions() Options {
@@ -62,6 +78,14 @@ func getCmdOptions() Options {
 	flag.StringVar(&opt.ServerAddress, "a", "localhost:8080", "port on which the server should run")
 	flag.Var(&opt.ReportInterval, "r", "how often console utility should send metrics")
 	flag.Var(&opt.PollInterval, "p", "how often console utility should poll metrics")
+
+	flag.Var(&opt.StoreInterval, "i", "интервал времени в секундах, по истечении которого"+
+		" текущие показания сервера сохраняются на диск (по умолчанию 300 секунд, значение 0 делает запись синхронной)")
+	flag.StringVar(&opt.FileStoragePath, "f", "storage.dat", "путь до файла, куда "+
+		"сохраняются текущие значения. Имя файла для значения по умолчанию придумайте сами.")
+	flag.BoolVar(&opt.Restore, "r", false, "булево значение (true/false), определяющее, "+
+		"следует ли загружать ранее сохранённые значения из указанного файла при старте сервера")
+
 	flag.Parse()
 	return opt
 }
