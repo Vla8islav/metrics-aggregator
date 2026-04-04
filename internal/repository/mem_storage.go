76474005331081839b3af9758fd751cc9d30af1c
vla8islav <vladislav888@gmail.com>
2026-04-04 13:00:35 +0300

    iter12 rolled back the letter for the report interval

--- internal/repository/mem_storage.go
+++ internal/repository/mem_storage.go
@@ -27,7 +27,7 @@ func NewMemStorage(config *config.Options) *MemoryStorage {
 }
 
 func (s *MemoryStorage) Ping(_ context.Context) error {
-	return nil
+	return fmt.Errorf("this is an in-memory storage, not a full-fledged DB")
 }
 
 func (s *MemoryStorage) Restore(ctx context.Context) error {
