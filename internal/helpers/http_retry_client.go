c2823bd6508ec0ca4e90a81185604ba038d30dee
vla8islav <vladislav888@gmail.com>
2026-04-09 09:28:18 +0300

    iter13 added some context

--- internal/helpers/http_retry_client.go
+++ internal/helpers/http_retry_client.go
@@ -1,34 +1,88 @@
 package helpers
 
 import (
+	"io"
 	"net/http"
-	"sync"
-
-	"github.com/hashicorp/go-cleanhttp"
+	"time"
 )
 
-type HTTPRetryClient struct {
-	client *http.Client
-
-	RetryCount int
-
-	clientInit sync.Once
+type HTTPDoRequester interface {
+	Do(req *http.Request) (*http.Response, error)
 }
 
-func NewHTTPRetryClient() *HTTPRetryClient {
-	return &HTTPRetryClient{client: &http.Client{}, RetryCount: 0, clientInit: sync.Once{}}
+type HTTPRetryClient struct {
+	client HTTPDoRequester
+
+	MaxAttempts  int
+	AttemptDelay time.Duration
+
+	shouldRetryOnStatus func(statusCode int) bool
+}
+
+func NewHTTPRetryClient(shouldRetryOnStatus func(int) bool) *HTTPRetryClient {
+	if shouldRetryOnStatus == nil {
+		shouldRetryOnStatus = defaultShouldRetryStatus
+	}
+	return &HTTPRetryClient{client: &http.Client{}, MaxAttempts: 1,
+		shouldRetryOnStatus: shouldRetryOnStatus}
+}
+
+func defaultShouldRetryStatus(statusCode int) bool {
+	switch statusCode {
+	case http.StatusTooManyRequests,
+		http.StatusBadGateway,
+		http.StatusServiceUnavailable,
+		http.StatusGatewayTimeout:
+		return true
+	default:
+		return false
+	}
 }
 
 func (c *HTTPRetryClient) Do(req *http.Request) (*http.Response, error) {
-	c.clientInit.Do(func() {
-		if c.client == nil {
-			c.client = cleanhttp.DefaultPooledClient()
-		}
-	})
 
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return nil, err
+	var resp *http.Response
+	var doErr error
+
+	for attempt := 1; attempt <= c.MaxAttempts; attempt++ {
+		// reset the request body
+		if req.GetBody != nil {
+			body, getBodyErr := req.GetBody()
+			if getBodyErr != nil {
+				return nil, getBodyErr
+			}
+			req.Body = body
+
+		}
+		resp, doErr = c.client.Do(req)
+		if doErr == nil {
+			shouldRetryStatus := c.shouldRetryOnStatus
+			if shouldRetryStatus == nil {
+				shouldRetryStatus = defaultShouldRetryStatus
+			}
+
+			if !shouldRetryStatus(resp.StatusCode) || attempt >= c.MaxAttempts {
+				return resp, nil
+			}
+
+			_, _ = io.Copy(io.Discard, resp.Body)
+			_ = resp.Body.Close()
+
+		} else if attempt >= c.MaxAttempts {
+			return resp, doErr
+		}
+
+		timer := time.NewTimer(c.AttemptDelay)
+		select {
+		case <-req.Context().Done():
+			if !timer.Stop() {
+				<-timer.C
+			}
+			return nil, req.Context().Err()
+		case <-timer.C:
+		}
+
 	}
-	return resp, nil
+
+	return resp, doErr
 }
