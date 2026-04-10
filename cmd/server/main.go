093970c18d0b89f3ed8c6ad0d1d6caeab6eda6eb
vla8islav <vladislav888@gmail.com>
2026-04-11 00:06:08 +0300

    iter13 moved DB init logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/Vla8islav/metrics-aggregator/internal/config"
+	"github.com/Vla8islav/metrics-aggregator/internal/domain"
 	"github.com/Vla8islav/metrics-aggregator/internal/handler"
 	"github.com/Vla8islav/metrics-aggregator/internal/middlewares"
+	"github.com/Vla8islav/metrics-aggregator/internal/repository"
 	"github.com/Vla8islav/metrics-aggregator/internal/service"
 	"go.uber.org/zap"
 )
@@ -23,10 +26,10 @@ func main() {
 	currentConfig := config.ReadFlags(os.Args[1:])
 	logger.Info("starting server ", zap.String("Server addr", currentConfig.ServerAddress.Value))
 
-	db, err := initRepository(currentConfig, logger)
+	var db domain.MetricRepository
+	db, err = getDB(currentConfig, logger)
 	if err != nil {
-		logger.Fatal("failed to initialize metrics repository", zap.Error(err))
-		return
+		logger.Fatal("failed to initialize db", zap.Error(err))
 	}
 
 	srvApp := service.NewMetricsService(db)
@@ -51,4 +54,36 @@ func main() {
 		return
 	}
 
+}
+
+func getDB(currentConfig *config.Options, logger *zap.Logger) (domain.MetricRepository, error) {
+	var db domain.MetricRepository
+	var err error
+
+	if currentConfig.DatabaseDSN.BeenSet {
+		db, err = repository.NewPostgresStorage(currentConfig, currentConfig.MigrationsFolder.Value)
+		if err != nil {
+			logger.Fatal("failed to initialize metrics repository", zap.Error(err))
+			return nil, nil
+		}
+	} else if !currentConfig.Restore.BeenSet || !currentConfig.Restore.Value {
+		logger.Info("making a fresh in-memory DB because the restore flag is false")
+		db = repository.NewMemStorage(currentConfig)
+	} else if currentConfig.Restore.Value {
+		logger.Info("trying to load data from file into the in-memory DB")
+		dbInMemory := repository.NewMemStorage(currentConfig)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		go dbInMemory.RunSaver(ctx)
+		err = dbInMemory.Restore(ctx)
+		if err != nil {
+			logger.Fatal("failed to restore database", zap.Error(err))
+		}
+		db = dbInMemory
+	} else {
+		logger.Fatal("something strange happened: " +
+			"restore and connection string parameters are incorrect. Exiting...")
+		return nil, nil
+	}
+	return db, err
 }
