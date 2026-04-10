2042bcd85d8e99a5c1baaed6dfda3ebaa3a7a50c
vla8islav <vladislav888@gmail.com>
2026-04-11 00:48:44 +0300

    iter13 removed some seemingly redundant methods

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -12,7 +11,6 @@ import (
 	"github.com/Vla8islav/metrics-aggregator/internal/domain"
 	"github.com/Vla8islav/metrics-aggregator/internal/handler"
 	"github.com/Vla8islav/metrics-aggregator/internal/middlewares"
-	"github.com/Vla8islav/metrics-aggregator/internal/repository"
 	"github.com/Vla8islav/metrics-aggregator/internal/service"
 	"go.uber.org/zap"
 )
@@ -31,7 +29,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	db, err = getDB(ctx, currentConfig, logger)
+	db, err = initDB(ctx, currentConfig, logger)
 	if err != nil {
 		logger.Fatal("failed to initialize db", zap.Error(err))
 	}
@@ -57,42 +55,4 @@ func main() {
 		logger.Fatal(err.Error())
 		return
 	}
-
-}
-
-func getDB(ctx context.Context, currentConfig *config.Options, logger *zap.Logger) (domain.MetricRepository, error) {
-	var db domain.MetricRepository
-	var err error
-
-	// Case 1
-	if currentConfig.DatabaseDSN.BeenSet {
-		db, err = repository.NewPostgresStorage(currentConfig, currentConfig.MigrationsFolder.Value)
-		if err != nil {
-			return nil, fmt.Errorf("failed to initialize metrics repository: %w", err)
-		}
-		return db, nil
-	}
-
-	// Case 2
-	if !currentConfig.Restore.BeenSet || !currentConfig.Restore.Value {
-		logger.Info("making a fresh in-memory DB because the restore flag is false")
-		db = repository.NewMemStorage(currentConfig)
-		return db, nil
-	}
-
-	// Case 3
-	if currentConfig.Restore.Value {
-		logger.Info("trying to load data from file into the in-memory DB")
-		dbInMemory := repository.NewMemStorage(currentConfig)
-		go dbInMemory.RunSaver(ctx)
-		err = dbInMemory.Restore(ctx)
-		if err != nil {
-			return nil, fmt.Errorf("failed to restore database")
-		}
-		db = dbInMemory
-		return db, nil
-	}
-
-	return nil, fmt.Errorf("something strange happened: " +
-		"restore and connection string parameters are incorrect")
 }
