0aca7b1bd13cd0da3eb04c7d899984c94e3ab1cb
Vladislav Pilipenko <vladislav888@gmail.com>
2026-06-12 22:02:22 +0300

    iter18 added godoc

--- internal/handler/example_test.go
+++ internal/handler/example_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 
 	models "github.com/Vla8islav/metrics-aggregator/internal/model"
 	"go.uber.org/zap"
@@ -15,7 +16,8 @@ type fakeService struct{}
 func (fakeService) GetAll(ctx context.Context) (models.MetricsExport, error) {
 	return models.MetricsExport{
 		Gauges: map[string]float64{
-			"Alloc": 123.45,
+			"Alloc":       1.124,
+			"RandomValue": 42,
 		},
 		Counters: map[string]int64{
 			"PollCount": 10,
@@ -55,15 +57,60 @@ func ExampleNewRouter() {
 
 	router := NewRouter(h)
 
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	for _, request := range []struct {
+		method string
+		path   string
+	}{
+		{method: http.MethodGet, path: "/"},
+		{method: http.MethodGet, path: "/ping"},
+		{method: http.MethodGet, path: "/value/gauge/Alloc"},
+		{method: http.MethodPost, path: "/update/counter/PollCount/1"},
+	} {
+		req := httptest.NewRequest(request.method, request.path, nil)
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, req)
+		fmt.Println(request.method, request.path, w.Code)
+		if request.path == "/" {
+			fmt.Println(strings.Contains(w.Body.String(), "<h1>Metrics</h1>"))
+			fmt.Println(strings.Contains(w.Body.String(), "<strong>Alloc</strong>: 1.124"))
+			fmt.Println(strings.Contains(w.Body.String(), "<strong>RandomValue</strong>: 42"))
+			fmt.Println(strings.Contains(w.Body.String(), "<strong>PollCount</strong>: 10"))
+		}
+	}
+
+	// Output:
+	// GET / 200
+	// true
+	// true
+	// true
+	// true
+	// GET /ping 200
+	// GET /value/gauge/Alloc 200
+	// POST /update/counter/PollCount/1 200
+}
+
+func ExampleNewRouter_updateBatchMetrics() {
+	h := &Handler{
+		service: fakeService{},
+		logger:  zap.NewNop(),
+	}
+
+	router := NewRouter(h)
+
+	body := strings.NewReader(`[
+		{"id":"Alloc","type":"gauge","value":123.45},
+		{"id":"PollCount","type":"counter","delta":10}
+	]`)
+
+	req := httptest.NewRequest(http.MethodPost, "/updates", body)
+	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 
 	router.ServeHTTP(w, req)
 
 	fmt.Println(w.Code)
-	fmt.Println(w.Header().Get("Content-Type"))
 
 	// Output:
 	// 200
-	// text/html; charset=utf-8
 }
