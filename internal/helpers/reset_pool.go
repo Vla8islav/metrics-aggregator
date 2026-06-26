f0957b9a6bd64e5a19a59a086d916bfea864c0fc
Vladislav Pilipenko <vladislav888@gmail.com>
2026-06-26 08:55:08 +0300

    iter22 added Pool generic

---
+++ internal/helpers/reset_pool.go
@@ -0,0 +1,30 @@
+package helpers
+
+type ResetCapable interface {
+	Reset()
+}
+type Pool[T ResetCapable] struct {
+	items []T
+}
+
+func New[T ResetCapable]() *Pool[T] {
+	return &Pool[T]{}
+}
+
+func (p *Pool[T]) Get() T {
+	if len(p.items) == 0 {
+		var zero T
+		return zero
+	}
+
+	last := len(p.items) - 1
+	item := p.items[last]
+	p.items = p.items[:last]
+
+	return item
+}
+
+func (p *Pool[T]) Put(item T) {
+	item.Reset()
+	p.items = append(p.items, item)
+}
